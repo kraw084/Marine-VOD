285eeae8e6d97291eed87770962f7ea8ca3ba582
kraw084 <kraw084@aucklanduni.ac.nz>
2024-04-16 10:49:06 +1200

    Yolo lables/image folders

--- python/BrackishMOT.py
+++ python/BrackishMOT.py
@@ -4,6 +4,7 @@ import os
 import cv2
 import numpy as np
 import configparser
+import shutil
 from VOD_utils import Tracklet, display_VOD
 from Video_utils import frames_to_videos, video_to_frames
 
@@ -65,9 +66,45 @@ def play_brackish_video(video_number):
     display_VOD(frames, tracklets, NUM_TO_LABEL, 1080, 20)
 
 
+def create_yolo_images_and_labels():
+    for set in ("train", "test"):
+        path = "BrackishMOT/" + set
+        video_folders = os.listdir(path)
+        for video_folder in video_folders:
+            video_number = video_folder[-2:]
 
-#create_bMOT_videos()
+            for im in os.listdir(path + "/" + video_folder + "/" + "img1"):
+                im_full_path = path + "/" + video_folder + "/img1/" + im
+                shutil.copyfile(im_full_path, "BrackishMOT/images/vid" + video_number + "-" + im)
+                
+            txt = open(f"BrackishMOT/{set}/{video_folder}/gt/gt.txt")
+            lines = txt.readlines()
+            lines = [tuple([int(num.split(".")[0]) for num in line.strip("/n").split(",")][:-1]) for line in lines]
 
-#brackishMOT_tracklet(1)
-for i in range(10, 100):
-    play_brackish_video(i)
+            for frame, id, x_topleft, y_topleft, width, height, conf, label in lines:
+                if label > 5: continue
+
+                x = x_topleft + width//2
+                y = y_topleft + height//2
+
+                box = f"{label}, {x}, {y}, {width}, {height}"
+
+                label_path = f"BrackishMOT/labels/vid{video_number}-{frame:06}.txt"
+                if os.path.isfile(label_path):
+                    f = open(label_path, "a")
+                    f.write("\n" + box)
+                    f.close()
+                else:
+                    f = open(label_path, "w")
+                    f.write(box)
+                    f.close()
+
+
+if __name__ == "__main__":
+    #create_bMOT_videos()
+
+    #brackishMOT_tracklet(1)
+    #for i in range(10, 100):
+    #    play_brackish_video(i)
+
+    create_yolo_images_and_labels()
