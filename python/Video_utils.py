e6f5149cdaef56be65a268c44ea381aef5255fdf
kraw084 <kraw084@aucklanduni.ac.nz>
2024-04-16 12:49:22 +1200

    Created video class

--- python/Video_utils.py
+++ python/Video_utils.py
@@ -1,27 +1,6 @@
 import cv2
 
-def video_to_frames(video_file_path):
-    """Returns a list of image frames, size of frames, fps and fourcc code"""
-
-    frames = []
-    cap = cv2.VideoCapture(video_file_path)
-    size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))) #(w, h) of the image
-    fps = cap.get(cv2.CAP_PROP_FPS) #frame rate of the video
-    fourcc = int(cap.get(cv2.CAP_PROP_FOURCC)) #Fourcc video format code
-
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success: break
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frames.append(frame)
-
-    cap.release()
-
-    return frames, size, fps, fourcc
-
-
-def frames_to_videos(frames, video_file_path, fps, fourcc, size):
+def frames_to_video(frames, video_file_path, fps, size):
     """Saves a list of frames as a video"""
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     video = cv2.VideoWriter(video_file_path, fourcc, fps, size)
@@ -31,37 +10,6 @@ def frames_to_videos(frames, video_file_path, fps, fourcc, size):
         video.write(frame)
 
     video.release()
-
-
-def play_video(video_file_path, delay=0, delay_is_fps=False, size=640):
-    """Loads and plays a video frame by frame with delay millisconds beetween frame"""
-    cap = cv2.VideoCapture(video_file_path)
-
-    if delay_is_fps: delay = round(1000 * (1/delay))       
-
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success: break
-
-        frame = resize_image(frame, new_width=size) if frame.shape[1] > frame.shape[0] else resize_image(frame, new_height=size)
-        cv2.imshow(f"{video_file_path}", frame)
-        cv2.waitKey(delay)
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-def play_frame_by_frame(frames, delay=0, delay_is_fps=False, size=640):
-    """Plays a set of frames as a videowith delay millisconds beetween frame"""
-    if delay_is_fps: delay = round(1000 * (1/delay))       
-
-    for frame in frames:
-        frame = resize_image(frame, new_width=size) if frame.shape[1] > frame.shape[0] else resize_image(frame, new_height=size)
-        cv2.imshow(f"Video", frame)
-        cv2.waitKey(delay)
-
-    cv2.destroyAllWindows()
-
 
 def annotate_image(im, prediction, num_to_label, num_to_colour, draw_labels=True, ids=None):
         """Draws xywhcl boxes onto a single image. Colours are BGR"""
@@ -104,3 +52,59 @@ def resize_image(im, new_width=None, new_height=None):
         new_shape = (new_width, int(ratio * h))
 
     return cv2.resize(im, new_shape, cv2.INTER_AREA)
+
+
+class Video:
+    def __init__(self, video_file_path):
+        self.__name = video_file_path
+        self.__frames = []
+
+        cap = cv2.VideoCapture(video_file_path)
+        self.__size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))) #(w, h) of the image
+        self.__fps = cap.get(cv2.CAP_PROP_FPS) #frame rate of the video
+        self.__fourcc = int(cap.get(cv2.CAP_PROP_FOURCC)) #Fourcc video format code
+
+        while cap.isOpened():
+            success, frame = cap.read()
+            if not success: break
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.__frames.append(frame)
+
+        cap.release()
+
+        self.__num_of_frames = len(self.__frames)
+
+    def play(self, resize=640, fps=None):
+        if fps is None: fps = self.__fps
+        delay = round(1000 * (1/fps)) if fps != 0 else 0     
+
+        for frame in self.__frames:
+            frame = resize_image(frame, new_width=resize) if frame.shape[1] > frame.shape[0] else resize_image(frame, new_height=resize)
+            cv2.imshow(self.__name, frame)
+            cv2.waitKey(delay)
+
+        cv2.destroyAllWindows()
+
+    def save(self, video_file_path):
+        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+        video = cv2.VideoWriter(video_file_path, fourcc, self.__fps, self.__size)
+
+        for frame in self:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            video.write(frame)
+
+        video.release()
+
+    def __repr__(self):
+        return f"Video({self.__name})"
+    
+    def __iter__(self):
+        self.__i = 0
+        return self
+    
+    def __next__(self):
+        if self.__i > self.__num_of_frames: raise StopIteration
+        frame = self.__frames[self.__i]
+        self.__i += 1
+        return frame
