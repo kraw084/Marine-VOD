bf4b32189a7a33a6f8242471289053159e925670
kraw084 <kraw084@aucklanduni.ac.nz>
2024-04-16 15:23:40 +1200

    BrackishMOT set partitioning

--- python/BrackishMOT.py
+++ python/BrackishMOT.py
@@ -5,8 +5,15 @@ import cv2
 import numpy as np
 import configparser
 import shutil
+import random
+import math
+import sys
 from Video_utils import Video
 from VOD_utils import Tracklet, TrackletSet
+
+project_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_dir)
+import yolov5.train
 
 NUM_TO_LABEL = ["Jellyfish", "Fish", "Crab", "Shrimp", "Starfish", "Smallfish", ""]
 
@@ -99,11 +106,63 @@ def create_yolo_images_and_labels():
                     f.close()
 
 
+def train_test_split():
+    train = []
+    val = []
+    test = []
+
+    indices = list(range(1, 99))
+    random.shuffle(indices)
+
+    target_set = [0] * len(indices)
+    cutoff__1 = math.floor(len(indices)*0.8)
+    cutoff__2 = cutoff__1 + math.ceil(len(indices)*0.1)
+
+    for i, vid_index in enumerate(indices):
+        if i <= cutoff__1 and i <= cutoff__2: continue
+
+        if i <= cutoff__2: 
+            target_set[vid_index - 1] = 1
+        else:
+            target_set[vid_index - 1] = 2
+
+    for im in os.listdir("BrackishMOT/images"):
+        vid_number = int(im[3:5])
+        set_code = target_set[vid_number - 1]
+
+        if set_code == 0: train.append("images/" + im)
+        if set_code == 1: val.append("images/" + im)
+        if set_code == 2: test.append("images/" + im)
+
+    total = len(train + val + test)
+    print(len(train)/total, len(val)/total, len(test)/total)
+
+    for data, name in zip([train, val, test], ("train", "val", "test")):
+        f = open(f"BrackishMOT/{name}.txt", "w")
+        f.write("\n".join(data))
+        f.close()
+
+
+def train_brackish_detector():
+    yolov5.train.run(imgsz = 640, 
+                        epochs = 200, 
+                        data = "BrackishMOT/brackishMOT.yaml", 
+                        weights = "yolov5s.pt", 
+                        save_period = 10,
+                        batch_size = -1,
+                        cache = "ram",
+                        patience = 50,
+                        )
+
 if __name__ == "__main__":
     #create_bMOT_videos()
 
     #brackishMOT_tracklet(1)
-    for i in range(10, 100):
-        play_brackish_video(i)
+    #for i in range(10, 100):
+    #    play_brackish_video(i)
 
     #create_yolo_images_and_labels()
+    
+    #train_test_split()
+
+    train_brackish_detector()
